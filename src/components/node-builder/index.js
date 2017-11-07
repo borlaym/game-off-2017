679d8e5afc171c5858472e01b1c4f8f6165f5720
Marton Borlay <borlaym@gmail.com>
2017-11-07 21:37:22 +0100

    Add option input

--- src/components/node-builder/index.js
+++ src/components/node-builder/index.js
@@ -6,15 +6,17 @@ export default class NodeBuilder extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			value: ''
+			text: '',
+			options: ['']
 		};
-		this.handleChange = this.handleChange.bind(this);
+		this.handleTextChange = this.handleTextChange.bind(this);
 		this.renderSuggestion = this.renderSuggestion.bind(this);
+		this.addOption = this.addOption.bind(this);
 	}
 
-	handleChange(event, value, plainTextValue, mentions) {
+	handleTextChange(event, value, plainTextValue, mentions) {
 		this.setState({
-			value
+			text: value
 		}, () => this.props.onChange(value));
 	}
 
@@ -26,14 +28,31 @@ export default class NodeBuilder extends React.Component {
 		);
 	}
 
+	handleOptionChange(index, event, value) {
+		const newOptions = [
+			...this.state.options.slice(0, index),
+			value,
+			...this.state.options.slice(index + 1)
+		];
+		this.setState({
+			options: newOptions
+		});
+	}
+
+	addOption() {
+		this.setState({
+			options: [...this.state.options, '']
+		});
+	}
+
 	render() {
 		return (
 			<div className="node-builder">
 				<h3>Node text</h3>
 				<MentionsInput
 					className="text-input"
-					value={this.state.value}
-					onChange={this.handleChange}
+					value={this.state.text}
+					onChange={this.handleTextChange}
 					displayTransform={(id, display, type) => `[${display}]`}
 				>
 					<Mention trigger="["
@@ -41,6 +60,26 @@ export default class NodeBuilder extends React.Component {
 						renderSuggestion={this.renderSuggestion}
 					/>
 				</MentionsInput>
+				<h3>Options</h3>
+				{this.state.options.map((option, index) => (
+					<div key={index}>
+						<MentionsInput
+							className="option-input"
+							value={option}
+							onChange={this.handleOptionChange.bind(this, index)}
+							displayTransform={(id, display, type) => `[${display}]`}
+						>
+						<Mention trigger="["
+							data={this.props.tags}
+							renderSuggestion={this.renderSuggestion}
+						/>
+						</MentionsInput>
+
+					</div>
+				))}
+				<button className="add-option" onClick={this.addOption}>
+					Add option
+				</button>
 			</div>
 		);
 	}
