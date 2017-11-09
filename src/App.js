75e4bdb6ad2978494756fe034067f00efb84bfed
Oliver Kovacs <oliver@gizmodomedia.com>
2017-11-09 17:59:20 +0100

    feat(router): add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import firebase from './firebase';
 
 import logo from './logo.svg';
 import './App.css';
@@ -6,11 +8,39 @@ import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from './components/appbar';
 
+const Home = ({ user }) =>
+	user ? <p>Welcome {user.displayName}!</p> : <p>Please log in.</p>;
+
 class App extends Component {
+	state = {
+		user: null
+	};
+
+	componentDidMount() {
+		firebase.auth().onAuthStateChanged(user => {
+			if (user) {
+				this.setState({ user });
+			} else {
+				this.setState({ user: null });
+			}
+		});
+	}
+
 	render() {
 		return (
 			<MuiThemeProvider>
-				<AppBar />
+				<Router>
+					<div>
+						<AppBar />
+						{
+							<Route
+								exact
+								path="/"
+								render={() => <Home user={this.state.user} />}
+							/>
+						}
+					</div>
+				</Router>
 			</MuiThemeProvider>
 		);
 	}
