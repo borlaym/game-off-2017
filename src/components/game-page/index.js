b945f5a93165b2da5879a3b62102b428286baad9
Marton Borlay <borlaym@gmail.com>
2017-11-23 11:27:04 +0100

    feat(game-app): Resolve character

--- src/components/game-page/index.js
+++ src/components/game-page/index.js
@@ -1,18 +1,50 @@
+// @flow
+
 import * as React from 'react';
 import { withRouter } from 'react-router-dom';
 import Node from '../node';
 import Advanture from '../../adventure.json';
+import { resolveCharacter } from '../../utils/api';
+import { auth, isAuthenticated } from '../../firebase';
+import type { Character } from '../../types';
 
-class GamePage extends React.Component {
-	constructor(props) {
-		super(props);
+type State = {
+	character: ?Character
+};
+
+class GamePage extends React.Component<*, State> {
+	state = {
+		character: null
+	};
+
+	componentWillMount() {
+		const { history } = this.props;
+
+		if (!isAuthenticated()) {
+			history.push('/login');
+		}
+
+		auth.onAuthStateChanged((user) => {
+			if (user) {
+				resolveCharacter(user.uid)
+					.then((character) => {
+						this.setState({
+							character
+						});
+					})
+					.catch(() => history.push('/character/create'));
+			}
+		});
 	}
 
 	render() {
+		if (!this.state.character) {
+			return 'Loading...';
+		}
 		return (
 			<Node
 				node={Advanture['starter']}
-				character={{}}
+				character={this.state.character}
 				globalTags={[]}
 				onAction={action => console.log(action)}
 			/>
