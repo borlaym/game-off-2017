00b0fe95d785232c31ecfdc6f3ca81fa47152f6a
Marton Borlay <borlaym@gmail.com>
2017-11-23 12:28:18 +0100

    Resolve game state

--- src/resolveGameState.js
+++ src/resolveGameState.js
@@ -11,10 +11,45 @@ export default function({
 	party: Party,
 	player: Character
 }): GameState {
-	const globalTags: Array<Tag> = [];
+	let globalTags: Array<Tag> = [];
+	const resolvedPlayer = {
+		...player,
+		tags: []
+	};
 	let currentNode: Node = adventure.starter;
 	Object.keys(party.save).forEach((saveKey: string) => {
 		const saveStep = party.save[saveKey];
-		console.log(saveStep);
+		const node = adventure[saveStep._nodeRef];
+		const allActions = node.options.reduce((acc, option) => {
+			return [
+				...acc,
+				option.resultingAction
+			];
+		}, []);
+		const takenAction = allActions.find(_ => _.id === saveStep._actionID);
+		// Apply effects
+		if (takenAction.effects) {
+			(takenAction.effects.gainTags || []).forEach((effect) => {
+				if (effect.target === 'SELF') {
+					resolvedPlayer.tags.push(effect.tag);
+				} else {
+					globalTags.push(effect.tag);
+				}
+			});
+			(takenAction.effects.loseTags || []).forEach((effect) => {
+				if (effect.target === 'SELF') {
+					resolvedPlayer.tags = resolvedPlayer.tags.filter(_ => _ !== effect.tag);
+				} else {
+					globalTags = globalTags.filter(_ => _ !== effect.tag);
+				}
+			});
+		}
+		currentNode = adventure[takenAction.targetNode];
 	});
+	return {
+		globalTags,
+		characters: [],
+		player: resolvedPlayer,
+		currentNode
+	};
 }
