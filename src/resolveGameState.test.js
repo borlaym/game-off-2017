bd8e5e6a9270cd793fa2a7e5a160dddb5ee1ec5d
Marton Borlay <borlaym@gmail.com>
2017-11-23 14:38:42 +0100

    Add global tags as well

--- src/resolveGameState.test.js
+++ src/resolveGameState.test.js
@@ -45,10 +45,22 @@ describe('resolveGameState', () => {
 			save1: {
 				_nodeRef: 'starter',
 				_characterRef: 'player1',
-				_actionID: '1',
+				_actionID: '2',
 			},
 		}));
 		expect(gameState.characters).toMatchSnapshot();
 		expect(gameState.currentNode.id).toMatchSnapshot();
+	});
+
+	it('resolves an action that gives a global tag as well', () => {
+		const gameState = resolveGameState(valueWithSave({
+			save1: {
+				_nodeRef: 'starter',
+				_characterRef: 'player1',
+				_actionID: '1',
+			},
+		}));
+		expect(gameState.characters).toMatchSnapshot();
+		expect(gameState.globalTags).toMatchSnapshot();
 	});
 });
